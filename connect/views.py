e744e7a54bf9365d3207cc18bde35e10e05c6d0b
Pavlos Ratis <2286121R@student.gla.ac.uk>
2017-01-22 16:49:26 +0000

    create registration view

--- connect/views.py
+++ connect/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+
+from forms import UserForm
 
 def index(request):
     return render(request, "uofgconnect/index.html")
+
+def register(request):
+    context = RequestContext(request) # Get the context of the user request
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password) # Hash the password
+            user.save()
+            registered = True
+        else:
+            print user_form.errors
+    else:
+        user_form = UserForm()
+
+    return render_to_response(
+            'register.html',
+            {'user_form': user_form, 'registered': registered},
+            context)
