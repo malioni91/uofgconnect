5ba59748234bd20581ad5f4249131b87a8b9ef88
Pavlos Ratis <2286121R@student.gla.ac.uk>
2017-01-22 16:46:19 +0000

    add User model

--- connect/models.py
+++ connect/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Map(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    latitude = models.DecimalField(max_digits=9, decimal_places=5)
+    longitude = models.DecimalField(max_digits=9, decimal_places=5)
