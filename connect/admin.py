1e6037c09c819d59a00dfb18e3be8bf0bf0a334a
Nikolaos Theodosis <2272891T@student.gla.ac.uk>
2017-01-29 14:12:01 +0000

    User profile available in django admin

--- connect/admin.py
+++ connect/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from connect.models import Map, Course
+from connect.models import Map, Course, UserProfile
 
 # Register your models here.
 admin.site.register(Map)
 admin.site.register(Course)
+admin.site.register(UserProfile)
