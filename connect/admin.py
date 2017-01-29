22bb42afa59029f55845709105bffa0f1c38842e
Nikolaos Theodosis <2272891T@student.gla.ac.uk>
2017-01-29 13:26:30 +0000

    Courses available through admin panel

--- connect/admin.py
+++ connect/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from connect.models import Map
+from connect.models import Map, Course
 
 # Register your models here.
 admin.site.register(Map)
+admin.site.register(Course)
