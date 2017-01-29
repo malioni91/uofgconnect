9d656efa6def7d73c870f3be77451ae30bde76c7
Nikolaos Theodosis <2272891T@student.gla.ac.uk>
2017-01-29 17:16:54 +0000

    Fix model, form for userprofile

--- connect/forms.py
+++ connect/forms.py
@@ -47,8 +47,8 @@ class LoginForm(forms.ModelForm):
         fields = ('username', 'password')
 
 class UserProfileForm(forms.ModelForm):
-    # course = forms.ModelChoiceField(label="Course", queryset=Course.objects.all(),
-    #                                 widget=forms.Select(attrs={'class': 'form-control'}))
+    course = forms.ModelChoiceField(label="Course", queryset=Course.objects.all(),
+                                    widget=forms.Select(attrs={'class': 'form-control'}))
 
     class Meta:
         model = UserProfile
--- connect/models.py
+++ connect/models.py
@@ -18,7 +18,7 @@ class Course(models.Model):
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.OneToOneField(Course)
+    course = models.CharField(max_length=60)
 
     def __str__(self):
-        return self.course_name
+        return self.user.username
