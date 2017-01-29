fc75bb82a45fa7ad0149df7d51ed4524be02296e
Malion Hoxhallari <malioni91@gmail.com>
2017-01-29 13:17:29 +0000

    Added cookies to index

--- connect/views.py
+++ connect/views.py
@@ -5,10 +5,21 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from connect.forms import LoginForm, UserForm
+from datetime import datetime
 
 @login_required
 def index(request):
-    return render(request, "connect/index.html")
+    request.session.set_test_cookie()
+    context_dict = {}
+
+    visitor_cookie_handler(request)
+
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'connect/index.html', context=context_dict)
+    return response
+
+
 
 def landing(request):
     return render(request, "connect/landing.html")
@@ -53,3 +64,33 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/connect/')
+
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                                str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+# If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        #update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
