5ed3ed1366bd3ff652f86e6ca45abf8812359fc8
Nikolaos Theodosis <2272891T@student.gla.ac.uk>
2017-01-30 15:23:20 +0000

    Fix identation

--- connect/forms.py
+++ connect/forms.py
@@ -18,7 +18,7 @@ class UserForm(forms.ModelForm):
         fields = ('name', 'username', 'email', 'password', 'confirm_password')
 
     def clean_name(self):
-	full_name = self.cleaned_data.get('name').split()
+        full_name = self.cleaned_data.get('name').split()
         if len(full_name) == 1:
             self.instance.first_name = full_name[0]
         elif len(full_name) >= 3:
@@ -30,14 +30,14 @@ class UserForm(forms.ModelForm):
         return self.cleaned_data
 
     def clean(self):
-	super(UserForm, self).clean()
-	password = self.cleaned_data.get('password')
-	confirm_password = self.cleaned_data.get('confirm_password')
+        super(UserForm, self).clean()
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
         if not password:
             raise forms.ValidationError(mark_safe("Empty password. Try again."))
-	if password and password != confirm_password:
-	    raise forms.ValidationError(mark_safe("Passwords do not match. Try again."))
-	return self.cleaned_data
+        if password and password != confirm_password:
+            raise forms.ValidationError(mark_safe("Passwords do not match. Try again."))
+        return self.cleaned_data
 
 class LoginForm(forms.ModelForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
