c545b2fff2c28b61035bab5abfae9207f36f7b4c
Pavlos Ratis <2286121R@students.gla.ac.uk>
2017-03-21 01:04:54 +0000

    add basic support for pings

--- connect/urls.py
+++ connect/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from connect import views
+import notifications.urls
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -13,5 +14,8 @@ urlpatterns = [
     url(r'^users/$', views.users, name='users'),
     url(r'^pos_map/$', views.pos_map, name='pos_map'),
     url(r'^all_users/$', views.all_users, name='all_users'),
+    url(r'^uni_news/$', views.uni_news, name='uni_news'),
+    url(r'^notifications/', include(notifications.urls, namespace='notifications')),
     url(r'^notification/$', views.notification, name='notification'),
+    url(r'^messages/$', views.messages, name='messages'),
 ]
